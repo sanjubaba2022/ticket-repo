84e60c0ade5f45bf41c1772da2df5080330f2854
sanjeev khandelwal <khandelwal.digisol@gmail.com>
2022-12-14 22:07:09 +0530

    files added

---
+++ src/main/java/com/simplilearn/Ticket/Generator/TicketGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.simplilearn.Ticket.Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGeneratorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in the world of springboot");
+		SpringApplication.run(TicketGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Ticket/Generator/TicketGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Ticket.Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
